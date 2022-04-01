4f9b06d7ac74d8eb57f361df0d818d64529270d7
Daniil <porvipop@mail.ru>
2022-04-01 16:45:48 +0300

    Replace ImagePicker with manual take photo functionality

--- app/src/main/java/com/example/datingapp/ui/profilescreen/ProfileFragment.kt
+++ app/src/main/java/com/example/datingapp/ui/profilescreen/ProfileFragment.kt
@@ -2,62 +2,72 @@ package com.example.datingapp.ui.profilescreen
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import com.example.datingapp.R
+import androidx.core.content.FileProvider
+import androidx.fragment.app.Fragment
 import com.example.datingapp.databinding.FragmentProfileBinding
-import com.github.dhaval2404.imagepicker.ImagePicker
+import com.example.datingapp.util.Constants.FILE_NAME
+import com.example.datingapp.util.Constants.REQUEST_CODE
+import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
-
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var photoFile: File
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
 
 
         _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
 
         binding.cameraImageView.setOnClickListener {
-            ImagePicker.with(this)
-               /* .crop()	    			//Crop image(Optional), Check Customization for more option
-                .compress(1024)			//Final image size will be less than 1 MB(Optional)
-                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)*/
-                .start()
+
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            photoFile = getPhotoFile(FILE_NAME)
+
+            // This DOESN'T work for API >= 24 (starting 2016)
+            // takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+
+            val fileProvider = FileProvider.getUriForFile(requireContext(), "com.example.datingapp.fileprovider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+            startActivityForResult(takePictureIntent, REQUEST_CODE)
         }
 
         return binding.root
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK) {
-            //Image Uri will not be null for RESULT_OK
-            val uri: Uri = data?.data!!
 
-            // Use Uri object instead of File to avoid storage permissions
-            binding.profileImageView.setImageURI(uri)
-        } else if (resultCode == ImagePicker.RESULT_ERROR) {
-            Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            //val takenImage = data?.extras?.get("data") as Bitmap
+            val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+            binding.profileImageView.setImageBitmap(takenImage)
         } else {
-            Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
-
+    private fun getPhotoFile(fileName: String): File {
+        val storageDirectory = activity?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", storageDirectory)
+    }
 
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/datingapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.datingapp.util
+
+object Constants {
+    const val REQUEST_CODE = 42
+    const val FILE_NAME = "photo.jpg"
+}
