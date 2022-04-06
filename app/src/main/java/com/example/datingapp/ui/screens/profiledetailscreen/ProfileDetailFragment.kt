84c977b4611db84df1bd813e832a9d4701238c66
Daniil <porvipop@mail.ru>
2022-04-06 09:49:07 +0300

    Implement formatted string

--- app/src/main/java/com/example/datingapp/ui/screens/profiledetailscreen/ProfileDetailFragment.kt
+++ app/src/main/java/com/example/datingapp/ui/screens/profiledetailscreen/ProfileDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.datingapp.R
 import com.example.datingapp.databinding.FragmentProfileBinding
 import com.example.datingapp.databinding.FragmentProfileDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ class ProfileDetailFragment : Fragment() {
     private var _binding: FragmentProfileDetailBinding? = null
     private val binding get() = _binding!!
 
+    private var years: Int? = 33
 
 
     override fun onCreateView(
@@ -27,9 +29,15 @@ class ProfileDetailFragment : Fragment() {
 
         _binding = FragmentProfileDetailBinding.inflate(layoutInflater, container, false)
 
+        binding.yearsTextView.text = getString(R.string.years, years)
 
 
-        return binding.root
+            return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
